fbaf1a675c4f1e0dee1f5e58f5518d404c2057f7
aniket965 <aniket965.as@gmail.com>
2021-10-13 20:08:33 +0530

    feat: deployed mainnet

--- src/addresses/index.ts
+++ src/addresses/index.ts
@@ -1,6 +1,39 @@
 import { constants } from '../constants'
 import { Address } from './type'
 export const addresses: { [chainId: number]: Address } = {
+  1: {
+    registry: '0x6D290609b3F5F02D52F28d97C75a443ED8564cBf',
+    [constants.bridges.Hop]: '0x2b42AFFD4b7C14d9B7C2579229495c052672Ccd3',
+    [constants.bridges.PolygonBridge]:
+      '0x8537307810fC40F4073A12a38554D4Ff78EfFf41',
+      [constants.bridges.ArbitrumBridge]:
+      '0x565810cbfa3Cf1390963E5aFa2fB953795686339',
+      [constants.bridges.Hyphen]:
+      '0xc317144DE60E6bC9455363bB09852C00bd14CD61',
+      [constants.bridges.AnySwapRouterV4]:
+      '0xbDf50eAe568ECef74796ed6022a0d453e8432410',
+      [constants.middleware.OneInch]:
+      '0x8f9EaEe5c5df888aBA3c1Ab19689a0660d042c6d',
+
+      routeIds: {
+        [constants.bridges.Hop]: 1,
+        [constants.bridges.PolygonBridge]: 2,
+        [constants.bridges.ArbitrumBridge]: 3,
+        [constants.bridges.Hyphen]: 4,
+        [constants.bridges.AnySwapRouterV4]: 5,
+        [constants.middleware.OneInch]: 6,
+      },
+    chainInfo: {
+      chainId: 1,
+      name: 'mainnet',
+      isL1: true,
+    },
+    extraData: {
+      ERC20PredicateProxy: '0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf',
+      ERC721PredicateProxy: '0xE6F45376f64e1F568BD1404C155e5fFD2F80F7AD',
+      RootChainManagerProxy: '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77',
+    },
+  },
   
   56: {
     registry: '0x0b6a733c770Ada091bFf20d60a569e96AD695d2f',
